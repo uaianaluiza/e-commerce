9f4ff74f5c53accda992f078ef0e0d54f957b175
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-03-11 19:59:21 -0300

    Criar classe CompraController

--- src/main/java/br/com/zup/Ecommerce/controllers/CompraController.java
+++ src/main/java/br/com/zup/Ecommerce/controllers/CompraController.java
@@ -1,4 +1,12 @@
 package br.com.zup.Ecommerce.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("compras/")
+
 public class CompraController {
+
+
 }

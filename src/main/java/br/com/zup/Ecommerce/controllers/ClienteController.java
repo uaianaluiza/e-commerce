4ee4d5b7e12b7d5d6a846148a46788a96b453eec
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-03-11 23:48:05 -0300

    Consertar endpoint na classe ClienteControler

--- src/main/java/br/com/zup/Ecommerce/controllers/ClienteController.java
+++ src/main/java/br/com/zup/Ecommerce/controllers/ClienteController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 @RestController
-@RequestMapping("/clientes/")
+@RequestMapping("clientes/")
 public class ClienteController {
 
     @Autowired
